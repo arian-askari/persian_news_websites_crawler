f86f2da47274d7bf33bfe8bde3093c1113d1eb8a
ArianAskari <diyardownload@gmail.com>
2018-10-24 22:27:22 +0330

    add aftabnews crawler

--- irna_influence/crawlers/aftabnews_crawler.py
+++ irna_influence/crawlers/aftabnews_crawler.py
@@ -7,20 +7,20 @@ from textblob import TextBlob
 import sys
 from unidecode import unidecode
 
-server_url = "https://www.tabnak.ir/fa/news/"
-path_log = "./log/tabnak.log"
+server_url = "http://aftabnews.ir/fa/news/"
+path_log = "./log/aftabnews.log"
 
 mongo_server = "localhost"
 mongo_port = 27017
 client = MongoClient(mongo_server, mongo_port)
 db = client['news_sites']
-news = db['tabnak']
+news = db['aftabnews']
 
 if len(sys.argv)>1:
     start = sys.argv[1]
     end = sys.argv[2]
 else:
-    f = open(path_log, "r+") #845147, 845829
+    f = open(path_log, "r+") #551349,551798
     start, end = str(f.read()).split(",")
 
 docs = []
@@ -33,39 +33,31 @@ for i in range(int(start), int(end)):
 
         soup = BeautifulSoup(content, "html.parser")
 
-        title = str(soup.select('h1.Htag')[0].getText().strip())
+        title = str(soup.select('h1.title')[0].getText().strip())
         subtitle = None
         if len(soup.select('div.subtitle'))>0:
             subtitle = str(soup.select('div.subtitle')[0].getText().strip())
         else:
             subtitle = ""
+
         body = str(soup.select('div.body')[0].getText().strip())
 
-        view_count = soup.select("div.news_hits")[0].getText().strip()
-        view_count = re.findall(r'\d+', view_count)[0]
-        if len(view_count)==0:
-            view_count = 0
-
         comments_count = soup.findAll("a", {"href": "#comments"})
-        if len(comments_count)>0:
+        if len(comments_count) > 0:
             comments_count = unidecode(comments_count[0].getText().strip())
+            comments_count = re.findall(r'\d+', comments_count)[0]
         else:
             comments_count = 0
 
-        date_shamsi, time = soup.select('sapn.fa_date')[0].getText().strip().split('-')
-        date_shamsi = str(date_shamsi)
-        time = str(time)
-        date_georgian = soup.select('span.en_date')[0].getText().strip()
+        date, time  = soup.select('div.news_pdate_c')[0].getText().strip().replace("تاریخ انتشار:","").split('-')
 
         doc = {
                 "title": title,
                 "body" : body,
                 "abstract": subtitle,
                 "time": time,
-                "date_shamsi": date_shamsi,
-                "date_georgian" : date_georgian,
+                "date_shamsi": date,
                 "comment_count": comments_count,
-                "view_count" : view_count,
                 "link": link
         }
         docs.append(doc)
